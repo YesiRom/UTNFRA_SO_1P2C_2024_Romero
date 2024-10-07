0dd0de1328cd324002ec9dc53711e03f767e980f
YesiRom <yesica.romi.romero@gmail.com>
2024-10-07 20:14:07 +0000

    ADD: Modifico el punto F

--- RTA_SCRIPT_Examen_20241007/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_F.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd UTNFRA_SO_1P2C_2024_Romero/RTA_ARCHIVOS_Examen_20241001
+cd UTNFRA_SO_1P2C_2024_Romero/RTA_ARCHIVOS_Examen_20241007
 echo "Mi IP Pública es: $(curl -s ifconfig.me)" > Filtro_Avanzado.txt
 echo "Mi usuario es: $(whoami)" >> Filtro_Avanzado.txt
 echo "El Hash de mi Usuario es: $(sudo awk -F: -v user=$(whoami) '$1 == user {print $2}' /etc/shadow" >> Filtro_Avanzado.txt
